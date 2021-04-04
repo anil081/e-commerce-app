fa8b20b844656f1b7610169f16088840170ee7f2
anil081 <anil.yadav081@gmail.com>
2021-04-04 14:37:43 +0530

    Initial structure of app

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Directory from '../components/directory/Directory';
+// import './homepage.css'
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <Directory/>
+        </div>
+    );
+}
+
+export default HomePage;
